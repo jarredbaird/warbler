efa2b8c871c3a4f7f586c1673a2d872f9ac4c82b
jarredbaird@gmail.com <jarredbaird@gmail.com>
2021-03-15 14:59:13 -0400

    finshed part 1...need to figure out exactly what g is and what @app.before_request means. Moving on to part 2 after that

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request, flash, redirect, session, g
 from flask_debugtoolbar import DebugToolbarExtension
 from sqlalchemy.exc import IntegrityError
 
-from forms import UserAddForm, LoginForm, MessageForm
+from forms import UserAddForm, UserEditForm, LoginForm, MessageForm
 from models import db, connect_db, User, Message
 
 CURR_USER_KEY = "curr_user"
@@ -215,7 +215,28 @@ def profile():
     """Update profile for current user."""
 
     # IMPLEMENT THIS
+    if not g.user:
+        flash("Access unauthorized.", "danger")
+        return redirect("/")
 
+    user = g.user
+    form = UserEditForm(obj=user)
+
+    if form.validate_on_submit():
+
+        if User.authenticate(user.username, form.password.data):
+            user.username = form.username.data
+            user.email = form.email.data
+            user.image_url = form.image_url.data or "/static/images/default-pic.png"
+            user.header_image_url = form.header_image_url.data or "/static/images/warbler-hero.jpg"
+            db.session.add(user)
+            db.session.commit()
+            flash(f"{user.username}'s profile has been updated", "success")
+            return redirect(f'/users/{user.id}')
+        
+        flash("Wrong password, please try again.", 'danger')
+    
+    return render_template('users/edit.html',form=form, user_id=user.id)
 
 @app.route('/users/delete', methods=["POST"])
 def delete_user():
@@ -298,6 +319,7 @@ def homepage():
         messages = (Message
                     .query
                     .order_by(Message.timestamp.desc())
+                    .filter(Message.user_id.in_([u.id for u in g.user.following]+[g.user.id]))
                     .limit(100)
                     .all())
 
--- forms.py
+++ forms.py
@@ -15,8 +15,19 @@ class UserAddForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     email = StringField('E-mail', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[Length(min=6)])
+    bio = StringField("Bio")
     image_url = StringField('(Optional) Image URL')
+    header_image_url = StringField('(Optional) Header Image URL')
 
+class UserEditForm(FlaskForm):
+    """Form for adding users."""
+
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    bio = StringField("Bio")
+    image_url = StringField('(Optional) Image URL')
+    header_image_url = StringField('(Optional) Header Image URL')
+    password = PasswordField('Password', validators=[Length(min=6)])
 
 class LoginForm(FlaskForm):
     """Login form."""
